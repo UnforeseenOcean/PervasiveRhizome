3f4e6a3201add403d04427f1edb7bfd7deee05d1
Ansh <lightnarcissus@gmail.com>
2016-03-17 11:08:59 -0400

    Added license

--- nodejs-chat-master/public/hollywood.js
+++
@@ -1,1 +0,0 @@
-window.names = ["Laurence do Rego", "Ben Strickland", "John Tye", "Michael Woodford", "M. N. Vijayakumar", "Vijay Pandhare", "Carmen Segarra", "Linda Almonte", "Jim Wetta", "Joseph Faltaous", "Steven Woodward", "Jaydeen Vincente","Robert Rudolph", "Hector Rosado","Robert Evan Dawitt", "Cathy Harris", "Cate Jenkins", "Ed Snowden", "Anat Kamm"];
